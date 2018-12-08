15cbf729d1a4dd813f58c70a4aac5fef885b4d7e
sw-yx <swyx@dontemail.me>
2018-12-08 17:11:38 +0800

    Updates

---
+++ precache-manifest.309080294d40bf77032337554c5634b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c6c9afa896489816e41",
+    "url": "/hooks/static/js/runtime~main.8c6c9afa.js"
+  },
+  {
+    "revision": "7586b24c2fcfb598956f",
+    "url": "/hooks/static/js/main.7586b24c.chunk.js"
+  },
+  {
+    "revision": "049ab20118e58ae12561",
+    "url": "/hooks/static/js/1.049ab201.chunk.js"
+  },
+  {
+    "revision": "7586b24c2fcfb598956f",
+    "url": "/hooks/static/css/main.b00b89dd.chunk.css"
+  },
+  {
+    "revision": "983d60535d4d04a3c39ff7c71c41b00c",
+    "url": "/hooks/index.html"
+  }
+];
