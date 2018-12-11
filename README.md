0fd0e8da45113b423374a9b7813072a91d3fa1a9
sw-yx <swyx@dontemail.me>
2018-12-12 00:26:13 +0800

    add remaining hooks i have

--- README.md
+++ README.md
@@ -13,10 +13,39 @@ npm install --save @swyx/hooks
 ## Usage
 
 ```tsx
-import * as React from 'react';
+// useProduceState demo
+const [state, setState] = useProduceState({
+  todos: [],
+  showMenu: false
+});
+const closeModal = e => {
+  setState(draft => (draft.showMenu = false));
+};
 
-import { useLoading } from '@swyx/hooks';
+// useLocalStorage
+const [item, setItem, removeItem] = useLocalStorage(
+  'yourspecialkeyhere',
+  callbackOnItemChange
+);
 
+// useInput demo
+export default function InputArea({ onSubmit, defaultValue }) {
+  const { setValue, ...inputProps } = useInput(defaultValue);
+  const handleNewTodoKeyDown = event => {
+    if (event.keyCode !== 13) return;
+    event.preventDefault();
+    var val = event.target.value.trim();
+    if (val) {
+      onSubmit(val);
+      setValue('');
+    }
+  };
+  return (
+    <input onKeyDown={handleNewTodoKeyDown} {...inputProps} autoFocus={true} />
+  );
+}
+
+// useLoading demo
 function App() {
   const [isLoading, load] = useLoading();
   const [state, setState] = React.useState('you shouldnt see this');
@@ -29,11 +58,18 @@ function App() {
 function pingAPI(time) {
   return new Promise(resolve => setTimeout(resolve, time));
 }
+
+// useKeyDown demo
+export default function Menu(props) {
+  useKeydown('Escape', () => props.showMenu && props.handleModalClose());
+  // ...
+}
 ```
 
 ## Video Docs:
 
 - useLoading: https://www.youtube.com/watch?v=gAr2Qqoa00c
+- useKeyDown: https://twitter.com/swyx/status/1058927958205681664
 
 ## Acknowledgements
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,41 @@
 import * as React from 'react';
+import produce from 'immer';
+
+export type Props = { text: string };
+
+export function useProduceState(initState: any, observer: any = noop) {
+  const [state, setState] = React.useState(initState);
+  const cb = (mutatorOrValue: any, next?: Function) => {
+    if (isFunction(mutatorOrValue)) {
+      // is a function, put it through immer
+      setState((s: any) => produce(s, d => void mutatorOrValue(d)));
+      observer(state);
+    } else {
+      // is a value
+      setState(mutatorOrValue);
+      observer(mutatorOrValue);
+    }
+    if (next) next(); // post setState callback
+  };
+  // return [state, useCallback(cb, [setState])];
+  return [state, cb];
+}
+
+// https://stackoverflow.com/questions/5999998/how-can-i-check-if-a-javascript-variable-is-function-type
+function isFunction(functionToCheck?: Function) {
+  return (
+    functionToCheck && {}.toString.call(functionToCheck) === '[object Function]'
+  );
+}
+
+export function useInput(initialValue = '', stateObserver: any = noop) {
+  const [value, setValue] = React.useState(initialValue);
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+    stateObserver(e.target.value);
+  };
+  return { value, onChange, setValue };
+}
 
 export function useLoading() {
   const [isLoading, setState] = React.useState(false);
@@ -10,3 +47,85 @@ export function useLoading() {
   };
   return [isLoading, load];
 }
+
+export function useKeydown(key: string, handler: Function) {
+  React.useEffect(
+    () => {
+      const cb = (e: KeyboardEvent) => e.key === key && handler(e);
+      document.body.addEventListener('keydown', cb);
+      return () => {
+        document.body.removeEventListener('keydown', cb);
+      };
+    },
+    [key, handler]
+  );
+}
+
+export function useLocalStorage(key: string, optionalCallback: any = noop) {
+  const [state, setState] = React.useState(null);
+  React.useEffect(() => {
+    // chose to make this async
+    const existingValue = localStorage.getItem(key);
+    if (existingValue) {
+      const parsedValue = JSON.parse(existingValue);
+      setState(parsedValue);
+      optionalCallback(parsedValue);
+    }
+  }, []);
+  const removeItem = () => {
+    setState(null);
+    localStorage.removeItem(key);
+    optionalCallback(null);
+  };
+  const setItem = (obj: any) => {
+    setState(obj);
+    localStorage.setItem(key, JSON.stringify(obj));
+    optionalCallback(obj);
+  };
+  return [state, setItem, removeItem];
+}
+
+// utils
+
+function noop() {}
+
+// export function useOptimisticState(initState) {
+//   const [state, setState] = useState(initState);
+//   const oldState = useRef(state);
+//   function optimisticSetState(nextState) {
+//     oldState.current = state;
+//     setState(nextState);
+//   }
+//   async function tryAPI(somePromise) {
+//     return async function(yay, nay) {
+//       try {
+//         yay(optimisticSetState);
+//         return await somePromise;
+//       } catch (err) {
+//         nay(err);
+//         setState(oldState.current);
+//         return err;
+//       }
+//     };
+//   }
+//   return [state, tryAPI];
+// }
+
+// // usage
+// const [state, tryAPI] = useOptimisticState({ count: 0})
+// const success = setState => setState({ count : state.count + 1 })
+// const failure = error => console.log('Error: ', error)
+// const onClick = () => tryAPI(api.plusOne())(success, failure)
+
+// export default function App() {
+//   const [state, setState] = useProduceState({ foo: 1, bar: 2 });
+//   return (
+//     <div>
+//       <h1>setoldstate</h1>
+//       <div>{JSON.stringify(state)}</div>
+//       <button onClick={() => setState(draft => void (draft.foo = 3))}>
+//         test
+//       </button>
+//     </div>
+//   );
+// }
